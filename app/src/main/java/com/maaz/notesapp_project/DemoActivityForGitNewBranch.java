8d105ec2ba93e66884577445dfd03657900a0fb8
Maaz <maazsayyad2206@gmail.com>
2023-04-07 16:38:42 +0530

    NewFeature Branch Commit. New activity_demo_for_git_new_branch.xml Activity Added. Internet Permission Added.

---
+++ app/src/main/java/com/maaz/notesapp_project/DemoActivityForGitNewBranch.java
@@ -0,0 +1,14 @@
+package com.maaz.notesapp_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DemoActivityForGitNewBranch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_for_git_new_branch);
+    }
+}
